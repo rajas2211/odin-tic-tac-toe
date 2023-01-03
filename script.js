8c98ca81f650e4c1bf86e3a1374d37aaf5c19889
Rajas Shah <rajas.s.shah@gmail.com>
2023-01-03 13:10:05 -0700

    Complete Game logic

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable no-console */
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-underscore-dangle */
@@ -9,15 +10,41 @@ const Game = (() => {
         _gameState = new Array(9).fill(null);
         _gameOver = false;
         _winner = null;
-        // eslint-disable-next-line no-use-before-define
         Display.reset();
-        // eslint-disable-next-line no-use-before-define
         Control.reset();
     };
     const getGameState = () => _gameState;
     const getGridState = (position) => _gameState[position];
     const getWinner = () => _winner;
-    const isGameOver = () => {
+    const checkWinner = () => {
+        const OnCondition = (subGridPositions) => {
+            // console.log(`Checking  for winner on ${subGridPositions}`)
+            const subGrid = [_gameState[subGridPositions[0]],
+                             _gameState[subGridPositions[1]],
+                             _gameState[subGridPositions[2]]];
+            if (subGrid.every(grid => grid === Control.getPlayer1())){
+                return Control.getPlayer1();
+            }
+            if (subGrid.every(grid => grid === Control.getPlayer2())){
+                return Control.getPlayer2();
+            }
+                return false;
+        }
+        const conditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                            [0, 4, 8], [2, 4, 6]];
+        conditions.forEach(condition => {
+            const isWinner = OnCondition(condition);
+            if (isWinner){
+                _winner = isWinner;
+            }
+        });
+        if (_winner) console.log(`${_winner.getMarker()} WINS!`);
+        return _winner;
+    };
+    const isOver = () => {
+        if (_gameOver) return _gameOver;
+        checkWinner();
         if (!_gameState.includes(null) || _winner) {
             _gameOver = true;
             console.log(`Game Over`);
@@ -25,47 +52,55 @@ const Game = (() => {
         return _gameOver;
     };
     const setWinner = player => {_winner = player};
-    const finishGame = () => {_gameOver = true};
+    const finish = () => {Display.declareWinner(_winner)};
     const play = (position, player) => {
         _gameState[position] = player;
-        // eslint-disable-next-line no-use-before-define
         Display.play(position, player);
     };
     return {
         getGameState, getGridState,
-        reset, isGameOver, finishGame,
+        reset, isOver, finish,
         getWinner, setWinner,
         play
     };
 })();
 
 const Display = (() => {
+    const grids = document.querySelectorAll(`div.grid`);
+    const winner = document.querySelector(`h2.winner`);
     const reset = () => {
-        const grids = document.querySelectorAll(`div.grid`);
         grids.forEach(grid => {
             grid.textContent = null;
         });
-        const winner = document.querySelector(`p.winner`);
         winner.textContent = null;
         // winner.textContent = `${Game.getWinner()} wins!`;
     };
     const play = (position, player) => {
         const grid = document.querySelector(`#grid-${position}`);
-        grid.textContent = player.getPlayerMarker();
+        grid.textContent = player.getMarker();
     };
-    return {reset, play}
+    const declareWinner = (player) => {
+        if (player) {
+            winner.textContent = `${player.getMarker()} WINS!`;
+        }
+        else {
+            winner.textContent = `It's a DRAW!`;
+        }
+
+    }
+    return {reset, play, declareWinner}
 })();
 
 const Player = (marker, name=null) => {
-    const getPlayerMarker = () => marker;
-    const getPlayerName = () => name;
-    const setPlayerName = newName => {
+    const getMarker = () => marker;
+    const getName = () => name;
+    const setName = newName => {
         name=newName;
     };
     const setPlayerMarker = newMarker => {
         marker = newMarker;
     }
-    return {getPlayerMarker, getPlayerName, setPlayerMarker, setPlayerName};
+    return {getMarker, getName, setPlayerMarker, setName};
 };
 
 const Control = (() => {
@@ -85,13 +120,13 @@ const Control = (() => {
     };
     const play = (event) => {
         const position = event.target.id[event.target.id.length - 1];
-        if (!Game.isGameOver()){
+        if (!Game.isOver()){
             if (!Game.getGridState(position)) {
-                console.log(`Move will be made by ${_currentPlayer.getPlayerMarker()}`);
+                // console.log(`Move will be made by ${_currentPlayer.getMarker()}`);
                 Game.play(position, _currentPlayer);
-                console.log(`Move made by ${_currentPlayer.getPlayerMarker()}`);
+                // console.log(`Move made by ${_currentPlayer.getMarker()}`);
                 toggleCurrentPlayer();
-                Game.isGameOver();
+                if (Game.isOver()) Game.finish();
             }
         }
     };
@@ -104,6 +139,8 @@ const Control = (() => {
     const reset = () => {
         _currentPlayer=_player1;
     };
-    return {grids, reset};
+    const getPlayer1 = () => _player1;
+    const getPlayer2 = () => _player2;
+    return {grids, reset, getPlayer1, getPlayer2};
 })();
 
